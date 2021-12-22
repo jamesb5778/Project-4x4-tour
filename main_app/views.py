bc8b8f3b35255c025ce44d8e58f24fb19c88504d
jamesb5778 <jamesb5778@gmail.com>
2021-12-22 10:05:03 -0700

    create home page template

--- main_app/views.py
+++ main_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
-class Home(View):
+class Home(TemplateView):
+    template_name = "home.html"
 
-    def get(self, request):
-        return HttpResponse("4x4 Home Page")
 
 class Profile(View):
 
